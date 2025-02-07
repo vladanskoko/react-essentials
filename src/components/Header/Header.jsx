3a88e3a625aba7b2c1a39431df614cccc0be7684
Vladan Skoko <vladan.skoko@axiomq.com>
2025-02-07 14:36:04 +0100

    Refactor App component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,23 @@
 import reactImg from "../../assets/react-core-concepts.png";
-import './Header.css'
+import "./Header.css";
 
-const reactDescriptions = ['Fundamental', 'Crutial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crutial", "Core"];
 
 function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1))
+  return Math.floor(Math.random() * (max + 1));
 }
 
 export default function Header() {
-    const description = reactDescriptions[genRandomInt(2)];
-    
-    return (
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-    );
-  }
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
