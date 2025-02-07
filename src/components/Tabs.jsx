fb61a1992e409b9198b273fa7564a23648b7da62
Vladan Skoko <vladan.skoko@axiomq.com>
2025-02-07 15:48:47 +0100

    Add Tabs component

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,9 @@
-export default function Tabs({ children, buttons }) {
+export default function Tabs({ children, buttons, buttonsContainer = 'menu' }) {
+  const ButtonContainer = buttonsContainer;
+  
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonContainer>{buttons}</ButtonContainer>
       {children}
     </>
   );
